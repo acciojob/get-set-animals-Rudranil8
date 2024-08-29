8a227a13a54ea2c06fd2dd08e9490bf36db9468e
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 20:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 //complete this code
-class Animal {
-	constructor(species) {
-        // Initialize the species property
-    }
+// class Animal {
+// 	constructor(species) {
+//     }
+// }
+
+// class Dog extends Animal {}
+
+// class Cat extends Animal {}
+
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  purr() {
+    console.log("purr");
+  }
 }
 
-class Dog extends Animal {}
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
 
-class Cat extends Animal {}
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
